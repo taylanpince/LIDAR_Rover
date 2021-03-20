080cd4795b3f4e6d5b7c795a1cdb571ac3426019
Taylan Pince <taylanpince@gmail.com>
2021-03-20 00:20:44 -0400

    Update diff drive controller to match new specs

--- ros/src/rover/nodes/rover_controller.py
+++ ros/src/rover/nodes/rover_controller.py
@@ -8,6 +8,8 @@ from std_msgs.msg import Int32
 from geometry_msgs.msg import Quaternion, Vector3
 from sensor_msgs.msg import Imu
 
+from rover.arduino import ArduinoController
+
 
 MOTOR_LEFT = b'L'
 MOTOR_RIGHT = b'R'
@@ -35,99 +37,6 @@ TOTAL_MAP_SCAN_POINTS = 360 * 3
 PORT_NAME = "/dev/ttyUSB0"
 
 
-class ArduinoController:
-    def __init__(self, conn):
-        self.conn = conn
-    
-    def extract_messages(self, payload):
-        messages = []
-        message = []
-        
-        for val in payload:
-            if val == INCOMING_MESSAGE_BEGIN:
-                message = []
-            elif val == INCOMING_MESSAGE_END:
-                if len(message) == 10:
-                    messages.append(message)
-            else:
-                message.append(val)
-        
-        return messages
-    
-    def parse_message(self, payload):
-        data_type = payload[0]
-
-        if data_type == INCOMING_DATA_TYPE_SCAN:
-            pos = int.from_bytes(payload[1:3], byteorder="big")
-            dist = int.from_bytes(payload[3:5], byteorder="big")
-        
-            return (data_type, (pos, dist))
-        elif data_type == INCOMING_DATA_TYPE_MOTOR:
-            lmotor_data = int.from_bytes(payload[1:3], byteorder="big", signed=True)
-            rmotor_data = int.from_bytes(payload[3:5], byteorder="big", signed=True)
-            
-            return (data_type, (lmotor_data, rmotor_data))
-        elif data_type == INCOMING_DATA_TYPE_QUATERNION:
-            w = int.from_bytes(payload[1:3], byteorder="big", signed=True)
-            x = int.from_bytes(payload[3:5], byteorder="big", signed=True)
-            y = int.from_bytes(payload[5:7], byteorder="big", signed=True)
-            z = int.from_bytes(payload[7:9], byteorder="big", signed=True)
-            
-            wf = float(w) / 16384.0
-            xf = float(x) / 16384.0
-            yf = float(y) / 16384.0
-            zf = float(z) / 16384.0
-            
-            return (data_type, (xf, yf, zf, wf))
-        elif data_type == INCOMING_DATA_TYPE_GYRO:
-            gx = int.from_bytes(payload[1:3], byteorder="big", signed=True)
-            gy = int.from_bytes(payload[3:5], byteorder="big", signed=True)
-            gz = int.from_bytes(payload[5:7], byteorder="big", signed=True)
-            
-            gxf = float(gx) * (4000.0 / 65536.0) * (math.pi / 180.0) * 25.0
-            gyf = float(gy) * (4000.0 / 65536.0) * (math.pi / 180.0) * 25.0
-            gzf = float(gz) * (4000.0 / 65536.0) * (math.pi / 180.0) * 25.0
-            
-            return (data_type, (gxf, gyf, gzf))
-        elif data_type == INCOMING_DATA_TYPE_ACCELEROMETER:
-            ax = int.from_bytes(payload[1:3], byteorder="big", signed=True)
-            ay = int.from_bytes(payload[3:5], byteorder="big", signed=True)
-            az = int.from_bytes(payload[5:7], byteorder="big", signed=True)
-            
-            axf = float(ax) * (8.0 / 65536.0) * 9.81
-            ayf = float(ay) * (8.0 / 65536.0) * 9.81
-            azf = float(az) * (8.0 / 65536.0) * 9.81
-            
-            return (data_type, (axf, ayf, azf))
-        
-        return None
-    
-    def read_incoming_data(self):
-        if self.conn.in_waiting < 12:
-            return []
-        
-        payload = list(self.conn.read_until(expected=bytes([INCOMING_MESSAGE_END]), size=24))
-        commands = []
-        
-        for message in self.extract_messages(payload):
-            command = self.parse_message(message)
-            
-            if command:
-                commands.append(command)
-        
-        return commands
-    
-    def send_button_command(self, command):
-        self.conn.write(command)
-        self.conn.write(COMMAND_END)
-    
-    def send_command(self, motor, direction, speed):
-        self.conn.write(motor)
-        self.conn.write(direction)
-        self.conn.write(str(speed).encode())
-        self.conn.write(COMMAND_END)
-
-
 class RoverController:
     def __init__(self):
         self.left_motor_pos = 0
@@ -138,6 +47,10 @@ class RoverController:
         self.last_orientation = None
         self.last_gyro = None
         self.last_acceleration = None
+        self.last_motor_command_time = 0
+        self.ori_cov = 0.0025 # Orientation covariance
+        self.vel_cov = 0.02 # Angular velocity covariance
+        self.acc_cov = 0.04 # Linear acceleration covariance
         
         self.serial_conn = serial.Serial(PORT_NAME, 57600, timeout=10)
         self.arduino = ArduinoController(self.serial_conn)
@@ -157,6 +70,12 @@ class RoverController:
         motor_direction = DIRECTION_FORWARDS if self.right_speed >= 0 else DIRECTION_BACKWARDS
         
         self.arduino.send_command(MOTOR_RIGHT, motor_direction, motor_power)
+
+    def send_motor_command(self, motor, speed):
+        motor_power = abs(speed)
+        motor_direction = DIRECTION_FORWARDS if speed >= 0 else DIRECTION_BACKWARDS
+
+        self.arduino.send_command(motor, motor_direction, motor_power)
         
     def publish_imu_message(self, publisher):
         if self.last_orientation is None or self.last_gyro is None or self.last_acceleration is None:
@@ -192,6 +111,16 @@ class RoverController:
         imu_msg.angular_velocity = angular_velocity
         imu_msg.linear_acceleration = linear_acceleration
         
+        imu_msg.orientation_covariance[0] = self.ori_cov
+        imu_msg.orientation_covariance[4] = self.ori_cov
+        imu_msg.orientation_covariance[8] = self.ori_cov
+        imu_msg.angular_velocity_covariance[0] = self.vel_cov
+        imu_msg.angular_velocity_covariance[4] = self.vel_cov
+        imu_msg.angular_velocity_covariance[8] = self.vel_cov
+        imu_msg.linear_acceleration_covariance[0] = self.acc_cov
+        imu_msg.linear_acceleration_covariance[4] = self.acc_cov
+        imu_msg.linear_acceleration_covariance[8] = self.acc_cov
+        
         publisher.publish(imu_msg)
         
         self.last_orientation = None
@@ -206,8 +135,6 @@ class RoverController:
         imu_publisher = rospy.Publisher('~imu_link', Imu, queue_size=10)
 
         self.max_motor_speed = int(rospy.get_param('~max_motor_speed'))
-
-        rate = rospy.Rate(10) # 10hz
         
         rospy.Subscriber('~lwheel_desired_rate', Int32, self.leftMotorCallback)
         rospy.Subscriber('~rwheel_desired_rate', Int32, self.rightMotorCallback)
@@ -230,11 +157,8 @@ class RoverController:
                 elif data_type == INCOMING_DATA_TYPE_ACCELEROMETER:
                     self.last_acceleration = payload
             
-            #rospy.loginfo("Wheels: Left {:03d} | Right {:03d}".format(left_motor_pos, right_motor_pos))
-
             self.publish_imu_message(imu_publisher)
-
-            rate.sleep()
+            self.send_motor_commands()
 
 
 if __name__ == '__main__':
---
+++ ros/src/rover/src/rover/arduino.py
@@ -0,0 +1,94 @@
+import math
+
+
+class ArduinoController:
+    def __init__(self, conn):
+        self.conn = conn
+    
+    def extract_messages(self, payload):
+        messages = []
+        message = []
+        
+        for val in payload:
+            if val == INCOMING_MESSAGE_BEGIN:
+                message = []
+            elif val == INCOMING_MESSAGE_END:
+                if len(message) == 10:
+                    messages.append(message)
+            else:
+                message.append(val)
+        
+        return messages
+    
+    def parse_message(self, payload):
+        data_type = payload[0]
+
+        if data_type == INCOMING_DATA_TYPE_SCAN:
+            pos = int.from_bytes(payload[1:3], byteorder="big")
+            dist = int.from_bytes(payload[3:5], byteorder="big")
+        
+            return (data_type, (pos, dist))
+        elif data_type == INCOMING_DATA_TYPE_MOTOR:
+            lmotor_data = int.from_bytes(payload[1:3], byteorder="big", signed=True)
+            rmotor_data = int.from_bytes(payload[3:5], byteorder="big", signed=True)
+            
+            return (data_type, (lmotor_data, rmotor_data))
+        elif data_type == INCOMING_DATA_TYPE_QUATERNION:
+            w = int.from_bytes(payload[1:3], byteorder="big", signed=True)
+            x = int.from_bytes(payload[3:5], byteorder="big", signed=True)
+            y = int.from_bytes(payload[5:7], byteorder="big", signed=True)
+            z = int.from_bytes(payload[7:9], byteorder="big", signed=True)
+            
+            wf = float(w) / 16384.0
+            xf = float(x) / 16384.0
+            yf = float(y) / 16384.0
+            zf = float(z) / 16384.0
+            
+            return (data_type, (xf, yf, zf, wf))
+        elif data_type == INCOMING_DATA_TYPE_GYRO:
+            gx = int.from_bytes(payload[1:3], byteorder="big", signed=True)
+            gy = int.from_bytes(payload[3:5], byteorder="big", signed=True)
+            gz = int.from_bytes(payload[5:7], byteorder="big", signed=True)
+            
+            gxf = float(gx) * (4000.0 / 65536.0) * (math.pi / 180.0) * 25.0
+            gyf = float(gy) * (4000.0 / 65536.0) * (math.pi / 180.0) * 25.0
+            gzf = float(gz) * (4000.0 / 65536.0) * (math.pi / 180.0) * 25.0
+            
+            return (data_type, (gxf, gyf, gzf))
+        elif data_type == INCOMING_DATA_TYPE_ACCELEROMETER:
+            ax = int.from_bytes(payload[1:3], byteorder="big", signed=True)
+            ay = int.from_bytes(payload[3:5], byteorder="big", signed=True)
+            az = int.from_bytes(payload[5:7], byteorder="big", signed=True)
+            
+            axf = float(ax) * (8.0 / 65536.0) * 9.81
+            ayf = float(ay) * (8.0 / 65536.0) * 9.81
+            azf = float(az) * (8.0 / 65536.0) * 9.81
+            
+            return (data_type, (axf, ayf, azf))
+        
+        return None
+    
+    def read_incoming_data(self):
+        if self.conn.in_waiting < 12:
+            return []
+        
+        payload = list(self.conn.read_until(expected=bytes([INCOMING_MESSAGE_END]), size=24))
+        commands = []
+        
+        for message in self.extract_messages(payload):
+            command = self.parse_message(message)
+            
+            if command:
+                commands.append(command)
+        
+        return commands
+    
+    def send_button_command(self, command):
+        self.conn.write(command)
+        self.conn.write(COMMAND_END)
+    
+    def send_command(self, motor, direction, speed):
+        self.conn.write(motor)
+        self.conn.write(direction)
+        self.conn.write(str(speed).encode())
+        self.conn.write(COMMAND_END)
